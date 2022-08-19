32a544ddd746f2451b9064e3a9279cbe72f04483
Ivan Gunchev <ivan@stadion.io>
2022-08-19 16:48:44 +0200

    feat: scoreboard from script

---
+++ pages/scoreboard.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const scoreboard = () => {
+
+    return (
+        <div id="scoreboard">
+            <Link href={"/"}>Home</Link>
+        </div>
+    )
+}
+
+export default scoreboard
